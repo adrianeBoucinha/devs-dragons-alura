a69dc2587a0fc6cb02ace33e9c57c8db70c01302
adrianeBoucinha <126793258+adrianeBoucinha@users.noreply.github.com>
2023-04-19 08:27:53 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { PersonagemView } from "./components/personagem-view.js"
+import { ArqueiroMago } from "./modules/arqueiro-mago.js";
+import { Arqueiro } from "./modules/arqueiro.js";
+import { Guerreiro } from "./modules/guerreiro.js";
+import { Mago } from "./modules/mago.js"
+import { Personagem } from "./modules/personagem.js"
+
+
+//const personagemPedrinho = new Personagem('Pedrinho', 5, 'Mago');
+//const personagemJose = new Personagem('Jose', 3, 'Arqueiro');
+
+const magoAntonio = new Mago ('Antonio', 4, 'fogo', 4, 3);
+const magaJulia = new Mago ('Julia', 8, 'gelo', 7, 10)
+const arqueiroBruno = new Arqueiro('Bruno', 7, 8)
+const arqueiroMagoChico = new ArqueiroMago('Chico', 7, 10, 'ar', 4, 8)
+const guerreiroJorge = new Guerreiro('Jorge', 8)
+
+const personagens = [magoAntonio, magaJulia, arqueiroBruno, arqueiroMagoChico, guerreiroJorge]
+new PersonagemView(personagens).render()
+
+//console.log(`Insigna de ${personagemPedrinho.nome}: ${personagemPedrinho.obterInsigna()}`)
+//console.log(`Insigna de ${personagemJose.nome}: ${personagemJose.obterInsigna()}`)
+
+//console.log(Personagem.verificarVencedor(arqueiroBruno, magoAntonio))
